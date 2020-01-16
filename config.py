6e1ce398c5919d3b30a91423ac940e6d25f47718
Erin Stuelke <erin_stuelke@med.unc.edu>
2020-01-16 13:56:00 -0500

    Moved config file to root directory

--- config.py
+++ config.py
@@ -9,3 +9,4 @@ load_dotenv(dotenv_path)
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    FRONTEND = os.getenv('FRONTEND')
