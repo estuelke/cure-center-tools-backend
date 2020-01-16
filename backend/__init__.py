3b73d23f278b2223e9679ce8dabf35b473d4d6d3
Erin Stuelke <erin_stuelke@med.unc.edu>
2020-01-16 14:01:35 -0500

    Added CORS support

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from backend.config import Config
+from config import Config
 
 db = SQLAlchemy()
 
@@ -8,6 +9,7 @@ db = SQLAlchemy()
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    CORS(app, resources={r'*': {'origins': Config.FRONTEND}})
 
     db.init_app(app)
 
