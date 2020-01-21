0a77817a17bcdd0d0cab50b05e35f0f42a022b1c
Erin Stuelke <erin_stuelke@med.unc.edu>
2020-01-21 10:39:58 -0500

    Added migration support for Flask

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app(config_class=Config):
@@ -12,7 +14,9 @@ def create_app(config_class=Config):
     CORS(app, resources={r'*': {'origins': Config.FRONTEND}})
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
+    # Routes/Blueprints
     from backend.members.routes import member_api
 
     app.register_blueprint(member_api)
--- config.py
+++ config.py
@@ -2,11 +2,13 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     FRONTEND = os.getenv('FRONTEND')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
