b1166bc629267ebbd39c437bb6a2dc928bf0583d
Erin Stuelke <erin_stuelke@med.unc.edu>
2020-01-27 16:46:54 -0500

    Using Marshmallow, created schemas for Members models, updated routes to reflect use of schemas

--- backend/members/routes.py
+++ backend/members/routes.py
@@ -1,16 +1,18 @@
 from flask import Blueprint, jsonify, request, make_response
 from .. import db
 from .models import Member
-
+from .schemas import MemberSchema
 
 member_api = Blueprint('members', __name__)
+member_schema = MemberSchema()
+members_schema = MemberSchema(many=True)
 
 
 @member_api.route('/members')
 def member_view():
-    member_list = [member.serialize for member in Member.query.all()]
-
-    return jsonify({'members': member_list})
+    members = Member.query.all()
+    result = members_schema.dump(members)
+    return jsonify({'members': result})
 
 
 @member_api.route('/add_member', methods=['POST'])
@@ -26,3 +28,11 @@ def add_member_view():
 
     response = make_response()
     return response
+
+
+@member_api.route('/member/<int:member_id>')
+def get_member(member_id):
+    member = Member.query.get_or_404(member_id)
+    result = member_schema.dump(member)
+
+    return jsonify({'member': result})
