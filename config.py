9b128d81b71ba9d4f2c58f592449c2d4548657f5
Erin Stuelke <erin_stuelke@med.unc.edu>
2020-03-06 17:44:18 -0500

    updated configurations for openshift deployment, added new libraries

--- config.py
+++ config.py
@@ -9,7 +9,11 @@ load_dotenv(dotenv_path)
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data/app.db')
     FRONTEND = os.getenv('FRONTEND')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SEED_FILE = os.environ.get('SEED_FILE')
+    MEMBER_SEED_FILE = os.environ.get('MEMBER_SEED_FILE')
+    COMPOUND_SEED_FILE = os.environ.get('COMPOUND_SEED_FILE')
+    FLASK_PORT = os.environ.get('FLASK_PORT', default=5000)
+    HOST = os.environ.get('HOST', default='127.0.0.1')
+    LOCAL = os.environ.get('LOCAL', default=False)
