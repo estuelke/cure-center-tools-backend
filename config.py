7b77429340d4f54ea46ed929c5392a84f0d06e77
Erin Stuelke <erin_stuelke@med.unc.edu>
2020-03-10 10:03:06 -0400

    set up for mongodb integration

--- config.py
+++ config.py
@@ -8,12 +8,25 @@ load_dotenv(dotenv_path)
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data/app.db')
-    FRONTEND = os.getenv('FRONTEND')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MEMBER_SEED_FILE = os.environ.get('MEMBER_SEED_FILE')
-    COMPOUND_SEED_FILE = os.environ.get('COMPOUND_SEED_FILE')
+    # Used for CORS configuration
+    FRONTEND = os.environ.get('FRONTEND')
+
+    # Environment-based
     FLASK_PORT = os.environ.get('FLASK_PORT', default=5000)
     HOST = os.environ.get('HOST', default='127.0.0.1')
     LOCAL = os.environ.get('LOCAL', default=False)
+
+    # SQLite3 Database
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, os.environ.get('SQLALCHEMY_DATABASE_NAME'))
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Mongo DB
+    MONGO_USER = os.environ.get('MONGO_USER')
+    MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD')
+    MONGO_DATABASE_NAME = os.environ.get('MONGO_DATABASE_NAME')
+    MONGO_DATABASE_ROUTE = os.environ.get('MONGO_DATABASE_ROUTE')
+    MONGO_DATABASE_PORT = os.environ.get('MONGO_DATABASE_PORT')
+
+    # Helpers
+    MEMBER_SEED_FILE = os.environ.get('MEMBER_SEED_FILE')
+    COMPOUND_SEED_FILE = os.environ.get('COMPOUND_SEED_FILE')
